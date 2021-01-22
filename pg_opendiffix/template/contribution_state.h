7a88a47d3d2b1015bb630b9283859e1224331026
Edon Gashi <egashi@mpi-sws.org>
2021-01-22 15:46:58 +0100

    Compute AID seed

--- pg_opendiffix/template/contribution_state.h
+++ pg_opendiffix/template/contribution_state.h
@@ -17,6 +17,9 @@
  * CS_CONTRIBUTION_COMBINE(a, b)
  * CS_OVERALL_CONTRIBUTION_INITIAL
  *
+ * --- Optional ---
+ * CS_INIT_ENTRY(state, entry) - callback to initialize hash table entries
+ *
  * --- What to yield? ---
  * CS_SCOPE
  * CS_DECLARE
@@ -31,6 +34,9 @@
 #include "postgres.h"
 #include "fmgr.h"
 
+#define CS_HASH_COMBINE(a, b) (a ^ b)
+#define CS_SEED_INITIAL 0
+
 #endif /* PG_OPENDIFFIX_CONTRIBUTION_STATE_H */
 
 /* ----------------------------------------------------------------
@@ -38,40 +44,25 @@
  * ----------------------------------------------------------------
  */
 
-/* Taken from "simplehash.h" */
+/* Utils (taken from "simplehash.h") */
 #define CS_MAKE_PREFIX(a) CppConcat(a, _)
 #define CS_MAKE_NAME(name) CS_MAKE_NAME_(CS_MAKE_PREFIX(CS_PREFIX), name)
 #define CS_MAKE_NAME_(a, b) CppConcat(a, b)
 
 /* Types for contributors */
-#define CS_CONTRIBUTOR CS_MAKE_NAME(Contributor)
+#define CS_TOP_CONTRIBUTOR CS_MAKE_NAME(Contributor)
 #define CS_CONTRIBUTION_STATE CS_MAKE_NAME(ContributionState)
-
-/* API Functions */
-#define CS_STATE_NEW CS_MAKE_NAME(state_new)
-#define CS_STATE_GETARG0 CS_MAKE_NAME(state_getarg0)
-#define CS_STATE_UPDATE CS_MAKE_NAME(state_update)
 
 /* Types for table */
 #define CS_TABLE CS_MAKE_NAME(Table)
 #define CS_TABLE_HASH CS_MAKE_NAME(Table_hash)
 #define CS_TABLE_ENTRY CS_MAKE_NAME(Table_Entry)
 
-/* Functions for table */
-#define CS_TABLE_CREATE CS_MAKE_NAME(Table_create)
-#define CS_TABLE_INSERT CS_MAKE_NAME(Table_insert)
-
-/* Internal functions */
-#define CS_INSERTION_INDEX CS_MAKE_NAME(internal_insertion_index)
-#define CS_AID_INDEX CS_MAKE_NAME(internal_aid_index)
-#define CS_INSERT_CONTRIBUTOR CS_MAKE_NAME(internal_insert_contributor)
-#define CS_BUMP_OR_INSERT_CONTRIBUTOR CS_MAKE_NAME(internal_bump_or_insert_contributor)
-/*
- * CS_BUMP_OR_INSERT_CONTRIBUTOR and CS_BUMP_CONTRIBUTOR are the same,
- * but CS_BUMP_CONTRIBUTOR can be optimized in the future by utilizing the
- * knowledge that the AID already exists in the top contributors list.
- */
-#define CS_BUMP_CONTRIBUTOR CS_BUMP_OR_INSERT_CONTRIBUTOR
+/* API Functions */
+#define CS_STATE_NEW CS_MAKE_NAME(state_new)
+#define CS_STATE_GETARG0 CS_MAKE_NAME(state_getarg0)
+#define CS_STATE_UPDATE_AID CS_MAKE_NAME(state_update_aid)
+#define CS_STATE_UPDATE_CONTRIBUTION CS_MAKE_NAME(state_update_contribution)
 
 /* ----------------------------------------------------------------
  * Declarations
@@ -82,9 +73,11 @@
 
 typedef struct CS_TABLE_ENTRY
 {
+  uint32 hash;
   CS_AID_TYPE aid;
   CS_CONTRIBUTION_TYPE contribution;
   char status;
+  bool has_contribution;
 } CS_TABLE_ENTRY;
 
 /* Hash table setup */
@@ -95,26 +88,29 @@ typedef struct CS_TABLE_ENTRY
 #define SH_KEY_TYPE CS_AID_TYPE
 #define SH_EQUAL(tb, a, b) CS_AID_EQUAL(a, b)
 #define SH_HASH_KEY(tb, key) CS_AID_HASH(key)
+#define SH_STORE_HASH
+#define SH_GET_HASH(tb, a) a->hash
 #define SH_SCOPE static inline
 #define SH_DECLARE
 #include "lib/simplehash.h"
 
-typedef struct CS_CONTRIBUTOR
+typedef struct CS_TOP_CONTRIBUTOR
 {
   CS_AID_TYPE aid;
   CS_CONTRIBUTION_TYPE contribution;
-} CS_CONTRIBUTOR;
+} CS_TOP_CONTRIBUTOR;
 
 typedef struct CS_CONTRIBUTION_STATE
 {
-  MemoryContext context;           /* Where the hash table lives */
   uint64 distinct_aids;            /* Number of distinct AIDs being tracked */
+  uint32 aid_seed;                 /* Seed derived from unique AIDs */
+  MemoryContext context;           /* Where the hash table lives */
   CS_TABLE_HASH *all_contributors; /* All contributors (map of AID -> TableEntry) */
 #ifdef CS_OVERALL_CONTRIBUTION_CALCULATE
   CS_CONTRIBUTION_TYPE overall_contribution; /* Total contribution from all contributors */
 #endif
-  unsigned int top_contributors_length;                   /* Length of top_contributors array */
-  CS_CONTRIBUTOR top_contributors[FLEXIBLE_ARRAY_MEMBER]; /* Stores top_contributors_length number of top contributors */
+  unsigned int top_contributors_length;                       /* Length of top_contributors array */
+  CS_TOP_CONTRIBUTOR top_contributors[FLEXIBLE_ARRAY_MEMBER]; /* Stores top_contributors_length number of top contributors */
 } CS_CONTRIBUTION_STATE;
 
 /* API Functions */
@@ -127,7 +123,11 @@ CS_SCOPE CS_CONTRIBUTION_STATE *CS_STATE_GETARG0(
     PG_FUNCTION_ARGS,
     unsigned int top_contributors_length);
 
-CS_SCOPE void CS_STATE_UPDATE(
+CS_SCOPE void CS_STATE_UPDATE_AID(
+    CS_CONTRIBUTION_STATE *state,
+    CS_AID_TYPE aid);
+
+CS_SCOPE void CS_STATE_UPDATE_CONTRIBUTION(
     CS_CONTRIBUTION_STATE *state,
     CS_AID_TYPE aid,
     CS_CONTRIBUTION_TYPE contribution);
@@ -143,6 +143,22 @@ CS_SCOPE void CS_STATE_UPDATE(
 
 #include "utils/elog.h"
 
+/* Functions for table */
+#define CS_TABLE_CREATE CS_MAKE_NAME(Table_create)
+#define CS_TABLE_INSERT_HASH CS_MAKE_NAME(Table_insert_hash)
+
+/* Internal functions */
+#define CS_INSERTION_INDEX CS_MAKE_NAME(internal_insertion_index)
+#define CS_AID_INDEX CS_MAKE_NAME(internal_aid_index)
+#define CS_INSERT_CONTRIBUTOR CS_MAKE_NAME(internal_insert_contributor)
+#define CS_BUMP_OR_INSERT_CONTRIBUTOR CS_MAKE_NAME(internal_bump_or_insert_contributor)
+/*
+ * CS_BUMP_OR_INSERT_CONTRIBUTOR and CS_BUMP_CONTRIBUTOR are the same,
+ * but CS_BUMP_CONTRIBUTOR can be optimized in the future by utilizing the
+ * knowledge that the AID already exists in the top contributors list.
+ */
+#define CS_BUMP_CONTRIBUTOR CS_BUMP_OR_INSERT_CONTRIBUTOR
+
 /* Hash table setup */
 
 #define SH_PREFIX CS_TABLE
@@ -151,6 +167,8 @@ CS_SCOPE void CS_STATE_UPDATE(
 #define SH_KEY_TYPE CS_AID_TYPE
 #define SH_EQUAL(tb, a, b) CS_AID_EQUAL(a, b)
 #define SH_HASH_KEY(tb, key) CS_AID_HASH(key)
+#define SH_STORE_HASH
+#define SH_GET_HASH(tb, a) a->hash
 #define SH_SCOPE static inline
 #define SH_DEFINE
 #include "lib/simplehash.h"
@@ -225,7 +243,7 @@ static inline void CS_INSERT_CONTRIBUTOR(
     memmove(
         &state->top_contributors[insertion_index + 1],
         &state->top_contributors[insertion_index],
-        elements * sizeof(CS_CONTRIBUTOR));
+        elements * sizeof(CS_TOP_CONTRIBUTOR));
   }
 
   state->top_contributors[insertion_index].aid = aid;
@@ -257,7 +275,7 @@ static inline void CS_BUMP_OR_INSERT_CONTRIBUTOR(
     memmove(
         &state->top_contributors[insertion_index + 1],
         &state->top_contributors[insertion_index],
-        elements * sizeof(CS_CONTRIBUTOR));
+        elements * sizeof(CS_TOP_CONTRIBUTOR));
   }
 
   state->top_contributors[insertion_index].aid = aid;
@@ -270,11 +288,12 @@ CS_SCOPE CS_CONTRIBUTION_STATE *CS_STATE_NEW(
     MemoryContext context,
     unsigned int top_contributors_length)
 {
-  CS_CONTRIBUTION_STATE *state = (CS_CONTRIBUTION_STATE *)
-      MemoryContextAlloc(context, sizeof(CS_CONTRIBUTION_STATE) + top_contributors_length * sizeof(CS_CONTRIBUTOR));
+  CS_CONTRIBUTION_STATE *state = (CS_CONTRIBUTION_STATE *)MemoryContextAlloc(
+      context, sizeof(CS_CONTRIBUTION_STATE) + top_contributors_length * sizeof(CS_TOP_CONTRIBUTOR));
 
-  state->context = context;
   state->distinct_aids = 0;
+  state->aid_seed = CS_SEED_INITIAL;
+  state->context = context;
   state->all_contributors = CS_TABLE_CREATE(context, 128, NULL);
   state->top_contributors_length = top_contributors_length;
 
@@ -304,16 +323,35 @@ CS_SCOPE CS_CONTRIBUTION_STATE *CS_STATE_GETARG0(
   return CS_STATE_NEW(agg_context, top_contributors_length);
 }
 
-CS_SCOPE void CS_STATE_UPDATE(
+CS_SCOPE void CS_STATE_UPDATE_AID(
+    CS_CONTRIBUTION_STATE *state,
+    CS_AID_TYPE aid)
+{
+  bool found;
+  uint32 aid_hash = CS_AID_HASH(aid);
+  CS_TABLE_ENTRY *entry = CS_TABLE_INSERT_HASH(state->all_contributors, aid_hash, aid, &found);
+  if (!found)
+  {
+    state->distinct_aids++;
+    state->aid_seed = CS_HASH_COMBINE(state->aid_seed, aid_hash);
+    entry->has_contribution = false;
+#ifdef CS_INIT_ENTRY
+    CS_INIT_ENTRY(state, entry);
+#endif
+  }
+}
+
+CS_SCOPE void CS_STATE_UPDATE_CONTRIBUTION(
     CS_CONTRIBUTION_STATE *state,
     CS_AID_TYPE aid,
     CS_CONTRIBUTION_TYPE contribution)
 {
   bool found;
+  uint32 aid_hash = CS_AID_HASH(aid);
+  CS_TABLE_ENTRY *entry = CS_TABLE_INSERT_HASH(state->all_contributors, aid_hash, aid, &found);
   unsigned int top_length = Min(state->distinct_aids, state->top_contributors_length);
   CS_CONTRIBUTION_TYPE contribution_old;
   CS_CONTRIBUTION_TYPE min_top_contribution;
-  CS_TABLE_ENTRY *entry = CS_TABLE_INSERT(state->all_contributors, aid, &found);
 
 #ifdef CS_OVERALL_CONTRIBUTION_CALCULATE
   state->overall_contribution = CS_CONTRIBUTION_COMBINE(state->overall_contribution, contribution);
@@ -321,14 +359,34 @@ CS_SCOPE void CS_STATE_UPDATE(
 
   if (!found)
   {
-    entry->contribution = contribution;
+    /* AID does not exist in table. */
     state->distinct_aids++;
+    state->aid_seed = CS_HASH_COMBINE(state->aid_seed, aid_hash);
+    entry->has_contribution = true;
+    entry->contribution = contribution;
+#ifdef CS_INIT_ENTRY
+    CS_INIT_ENTRY(state, entry);
+#endif
+
     /* We can insert to top contributors if either: */
     if (
         /* - top_contributors is not full */
-        top_length != state->top_contributors_length
+        top_length != state->top_contributors_length ||
         /* - contribution is greater than the lowest top contribution */
-        || CS_CONTRIBUTION_GREATER(contribution, state->top_contributors[top_length - 1].contribution))
+        CS_CONTRIBUTION_GREATER(contribution, state->top_contributors[top_length - 1].contribution))
+    {
+      CS_INSERT_CONTRIBUTOR(state, top_length, aid, contribution);
+    }
+
+    return;
+  }
+  else if (!entry->has_contribution)
+  {
+    /* AID exists but hasn't contributed yet. */
+    entry->has_contribution = true;
+    entry->contribution = contribution;
+    if (top_length != state->top_contributors_length ||
+        CS_CONTRIBUTION_GREATER(contribution, state->top_contributors[top_length - 1].contribution))
     {
       CS_INSERT_CONTRIBUTOR(state, top_length, aid, contribution);
     }
@@ -374,6 +432,17 @@ CS_SCOPE void CS_STATE_UPDATE(
   CS_BUMP_OR_INSERT_CONTRIBUTOR(state, top_length, aid, contribution_old, entry->contribution);
 }
 
+/* Functions for table */
+#undef CS_TABLE_CREATE
+#undef CS_TABLE_INSERT_HASH
+
+/* Internal functions */
+#undef CS_INSERTION_INDEX
+#undef CS_AID_INDEX
+#undef CS_INSERT_CONTRIBUTOR
+#undef CS_BUMP_OR_INSERT_CONTRIBUTOR
+#undef CS_BUMP_CONTRIBUTOR
+
 #endif /* CS_DEFINE */
 
 /* ----------------------------------------------------------------
@@ -392,6 +461,7 @@ CS_SCOPE void CS_STATE_UPDATE(
 #undef CS_CONTRIBUTION_EQUAL
 #undef CS_CONTRIBUTION_COMBINE
 #undef CS_OVERALL_CONTRIBUTION_INITIAL
+#undef CS_INIT_ENTRY
 #undef CS_SCOPE
 #undef CS_DECLARE
 #undef CS_DEFINE
@@ -402,26 +472,16 @@ CS_SCOPE void CS_STATE_UPDATE(
 #undef CS_MAKE_NAME_
 
 /* Types for contributors */
-#undef CS_CONTRIBUTOR
+#undef CS_TOP_CONTRIBUTOR
 #undef CS_CONTRIBUTION_STATE
-
-/* API Functions */
-#undef CS_STATE_NEW
-#undef CS_STATE_GETARG0
-#undef CS_STATE_UPDATE
 
 /* Types for table */
 #undef CS_TABLE
 #undef CS_TABLE_HASH
 #undef CS_TABLE_ENTRY
 
-/* Functions for table */
-#undef CS_TABLE_CREATE
-#undef CS_TABLE_INSERT
-
-/* Internal functions */
-#undef CS_INSERTION_INDEX
-#undef CS_AID_INDEX
-#undef CS_INSERT_CONTRIBUTOR
-#undef CS_BUMP_OR_INSERT_CONTRIBUTOR
-#undef CS_BUMP_CONTRIBUTOR
+/* API Functions */
+#undef CS_STATE_NEW
+#undef CS_STATE_GETARG0
+#undef CS_STATE_UPDATE_AID
+#undef CS_STATE_UPDATE_CONTRIBUTION
