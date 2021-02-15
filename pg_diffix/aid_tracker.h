fd9b4c0ae09dbfed7f249069b2cea681337da28d
Edon Gashi <egashi@mpi-sws.org>
2021-02-15 15:38:58 +0100

    Add simpler AID tracker

---
+++ pg_diffix/aid_tracker.h
@@ -0,0 +1,53 @@
+#ifndef PG_DIFFIX_AID_TRACKER_H
+#define PG_DIFFIX_AID_TRACKER_H
+
+#include "postgres.h"
+#include "fmgr.h"
+
+#include "pg_diffix/aid.h"
+
+typedef struct AidTrackerHashEntry
+{
+  uint32 hash; /* Cached key hash */
+  aid_t aid;   /* Entry key */
+  char status; /* Required for hash table */
+} AidTrackerHashEntry;
+
+/*
+ * Declarations for HashTable<aid_t, AidTrackerHashEntry>
+ */
+#define SH_PREFIX AidTracker
+#define SH_ELEMENT_TYPE AidTrackerHashEntry
+#define SH_KEY_TYPE aid_t
+#define SH_SCOPE extern
+#define SH_DECLARE
+#include "lib/simplehash.h"
+
+typedef struct AidTrackerState
+{
+  MakeAidFunc make_aid;     /* Function to get the AID from a Datum */
+  AidTracker_hash *aid_set; /* Hash set of all AIDs */
+  uint64 aid_seed;          /* Current AID seed */
+  bool hash_aids;           /* Should AIDs be hashed when combined in the seed? */
+} AidTrackerState;
+
+/*
+ * Creates a new empty state.
+ */
+extern AidTrackerState *aid_tracker_new(
+    MemoryContext context,
+    MakeAidFunc make_aid,
+    bool hash_aids,
+    uint64 initial_seed);
+
+/*
+ * Updates state with a new AID.
+ */
+extern void aid_tracker_update(AidTrackerState *state, Datum aid_datum);
+
+/*
+ * Gets or creates the aggregation state from the argument at the given index.
+ */
+extern AidTrackerState *get_aggregate_aid_tracker(PG_FUNCTION_ARGS, int index);
+
+#endif /* PG_DIFFIX_AID_TRACKER_H */
---
+++ src/aid.c
@@ -0,0 +1,48 @@
+#include "postgres.h"
+#include "catalog/pg_type.h"
+#include "common/hashfn.h"
+#include "utils/builtins.h"
+#include "utils/elog.h"
+
+#include "pg_diffix/aid.h"
+
+static aid_t make_int4_aid(Datum datum)
+{
+  return DatumGetUInt32(datum);
+}
+
+static aid_t make_int8_aid(Datum datum)
+{
+  return DatumGetUInt64(datum);
+}
+
+static aid_t make_text_aid(Datum datum)
+{
+  char *str = text_to_cstring(DatumGetTextPP(datum));
+  return hash_bytes((unsigned char *)str, strlen(str));
+}
+
+AidSetup setup_aid(Oid aid_type)
+{
+  AidSetup setup;
+  setup.aid_is_hash = false;
+
+  switch (aid_type)
+  {
+  case INT4OID:
+    setup.make_aid = make_int4_aid;
+    break;
+  case INT8OID:
+    setup.make_aid = make_int8_aid;
+    break;
+  case TEXTOID:
+    setup.make_aid = make_text_aid;
+    setup.aid_is_hash = true;
+    break;
+  default:
+    ereport(ERROR, (errmsg("Unsupported AID type")));
+    break;
+  }
+
+  return setup;
+}
