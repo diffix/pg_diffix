015d84030bec677602a5374f70f698605120d8d3
Cristian Berneanu <bcristi@gmail.com>
2021-03-19 14:56:07 +0200

    Tweak test data.

--- test/sql/tests.sql
+++ test/sql/tests.sql
@@ -14,7 +14,7 @@ INSERT INTO test_products VALUES (0, NULL, NULL), (1, 'Food', 1.5), (2, 'Car', 1
 CREATE TABLE test_purchases (cid INTEGER, pid INTEGER);
 INSERT INTO test_purchases VALUES (0, 0), (0, 1), (0, 3), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1),
   (7, 1), (8, 2), (9, 1), (10, 2), (11, 1), (12, 1), (13, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2),
-  (7, 1), (8, 1), (9, 2), (10, 2), (11, 2), (12, 1), (13, 0), (NULL, NULL);
+  (7, 1), (8, 1), (9, 2), (1, 2), (1, 2), (2, 1), (3, 0), (NULL, NULL);
 
 INSERT INTO diffix_config (rel_namespace_name, rel_name, aid_attname) VALUES
   ('public', 'test_customers', 'id'), ('public', 'test_purchases', 'cid');
