c809129eb635350df5ed5908e40d65405023332e
Cristian Berneanu <bcristi@gmail.com>
2021-03-30 10:31:52 +0300

    Allow marking databases with anonymization labels.

--- pg_diffix/auth.h
+++ pg_diffix/auth.h
@@ -15,6 +15,7 @@ extern AccessLevel get_access_level(void);
 /*
  * Returns true if the relation has been labeled as sensitive, false otherwise.
  * If the relation is unlabeled, the schema label is checked.
+ * If the schema is unlabeled, the database label is checked.
  */
 extern bool is_sensitive_relation(Oid relation_oid, Oid namespace_oid);
 
