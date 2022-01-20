2162f7da2c95ff4288cd95476d3960e5a52f7567
Cristian Berneanu <bcristi@gmail.com>
2022-01-20 21:17:50 +0200

    Rename `noise_seed` param to `salt`.

--- pg_diffix/config.h
+++ pg_diffix/config.h
@@ -9,7 +9,8 @@ typedef struct DiffixConfig
   int default_access_level;
   int session_access_level;
 
-  char *noise_seed;
+  char *salt;
+
   double noise_layer_sd;
 
   int low_count_min_threshold;
