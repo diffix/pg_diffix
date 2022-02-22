1eb71c3fd0b27016c737ee042346f09771df53b0
Cristian Berneanu <bcristi@gmail.com>
2022-02-22 13:30:11 +0200

    Split `count` and `count_any` aggregators into different modules.

--- src/aggregation/count_any.c
+++ src/aggregation/count_any.c
@@ -7,15 +7,16 @@
 #include "pg_diffix/query/anonymization.h"
 
 /* TODO: Implement aggregator methods. */
-const AnonAggFuncs g_count_funcs = {0};
+const AnonAggFuncs g_count_any_funcs = {0};
 
-static const int AIDS_OFFSET = 1;
+static const int VALUE_INDEX = 1;
+static const int AIDS_OFFSET = 2;
 
-PG_FUNCTION_INFO_V1(anon_count_transfn);
-PG_FUNCTION_INFO_V1(anon_count_finalfn);
-PG_FUNCTION_INFO_V1(anon_count_explain_finalfn);
+PG_FUNCTION_INFO_V1(anon_count_any_transfn);
+PG_FUNCTION_INFO_V1(anon_count_any_finalfn);
+PG_FUNCTION_INFO_V1(anon_count_any_explain_finalfn);
 
-Datum anon_count_transfn(PG_FUNCTION_ARGS)
+Datum anon_count_any_transfn(PG_FUNCTION_ARGS)
 {
   List *trackers = get_count_contribution_trackers(fcinfo, AIDS_OFFSET);
 
@@ -30,9 +31,13 @@ Datum anon_count_transfn(PG_FUNCTION_ARGS)
     if (!PG_ARGISNULL(aid_index))
     {
       aid_t aid = tracker->aid_descriptor.make_aid(PG_GETARG_DATUM(aid_index));
-      contribution_tracker_update_contribution(tracker, aid, one_contribution);
+      if (PG_ARGISNULL(VALUE_INDEX))
+        /* No contribution since argument is NULL, only keep track of the AID value. */
+        contribution_tracker_update_aid(tracker, aid);
+      else
+        contribution_tracker_update_contribution(tracker, aid, one_contribution);
     }
-    else
+    else if (!PG_ARGISNULL(VALUE_INDEX))
     {
       tracker->unaccounted_for++;
     }
@@ -41,15 +46,16 @@ Datum anon_count_transfn(PG_FUNCTION_ARGS)
   PG_RETURN_POINTER(trackers);
 }
 
-Datum anon_count_finalfn(PG_FUNCTION_ARGS)
+Datum anon_count_any_finalfn(PG_FUNCTION_ARGS)
 {
   List *trackers = get_count_contribution_trackers(fcinfo, AIDS_OFFSET);
   return count_calculate_final(fcinfo, trackers);
 }
 
-Datum anon_count_explain_finalfn(PG_FUNCTION_ARGS)
+Datum anon_count_any_explain_finalfn(PG_FUNCTION_ARGS)
 {
   List *trackers = get_count_contribution_trackers(fcinfo, AIDS_OFFSET);
   seed_t bucket_seed = compute_bucket_seed();
   return explain_count_trackers(bucket_seed, trackers);
 }
+
