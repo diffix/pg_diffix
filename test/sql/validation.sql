4883ffbe2caa94442ae4ac6a4fa605294f79a13d
Cristian Berneanu <bcristi@gmail.com>
2022-02-28 13:02:54 +0200

    Separate rejected queries tests from supported queries tests.

---
+++ test/sql/validation.sql
@@ -0,0 +1,114 @@
+SET pg_diffix.session_access_level = 'publish';
+
+----------------------------------------------------------------
+-- Supported queries
+----------------------------------------------------------------
+
+-- Supported functions for defining buckets
+SELECT COUNT(*) FROM empty_test_customers
+  GROUP BY substring(city, 1, 1);
+
+SELECT COUNT(*) FROM empty_test_customers
+  GROUP BY width_bucket(id, 0, 1000, 10), width_bucket(id::float, 0.0, 1000.0, 10);
+
+SELECT COUNT(*) FROM empty_test_customers
+  GROUP BY round(id::float, -1), round(id::numeric, -1);
+
+SELECT COUNT(*) FROM empty_test_customers
+  GROUP BY round(id::float), ceil(id::float), ceiling(id::float), floor(id::float);
+
+SELECT COUNT(*) FROM empty_test_customers
+  GROUP BY round(id::numeric), ceil(id::numeric), ceiling(id::numeric), floor(id::numeric);
+
+SELECT
+  diffix.round_by(id::numeric, 5),
+  diffix.round_by(id::double precision, 5),
+  COUNT(*)
+FROM empty_test_customers
+GROUP BY 1, 2;
+
+SELECT
+  diffix.ceil_by(id::numeric, 5),
+  diffix.ceil_by(id::double precision, 5),
+  COUNT(*)
+FROM empty_test_customers
+GROUP BY 1, 2;
+
+SELECT
+  diffix.floor_by(id::numeric, 5),
+  diffix.floor_by(id::double precision, 5),
+  COUNT(*)
+FROM empty_test_customers
+GROUP BY 1, 2;
+
+-- Allow all functions post-anonymization
+SELECT 2 * length(city) FROM empty_test_customers GROUP BY city;
+
+----------------------------------------------------------------
+-- Unsupported queries
+----------------------------------------------------------------
+
+-- Get rejected because aggregators are unsupported.
+SELECT SUM(id) FROM empty_test_customers;
+SELECT MIN(id) + MAX(id) FROM empty_test_customers;
+SELECT city FROM empty_test_customers GROUP BY 1 ORDER BY AVG(LENGTH(city));
+SELECT count(city ORDER BY city) FROM empty_test_customers;
+SELECT count(*) FILTER (WHERE true) FROM empty_test_customers;
+
+-- Get rejected because only a subset of expressions is supported for defining buckets.
+SELECT COUNT(*) FROM empty_test_customers GROUP BY LENGTH(city);
+SELECT COUNT(*) FROM empty_test_customers GROUP BY city || 'xxx';
+SELECT LENGTH(city) FROM empty_test_customers;
+SELECT city, 'aaaa' FROM empty_test_customers GROUP BY 1, 2;
+SELECT COUNT(*) FROM empty_test_customers GROUP BY round(floor(id));
+SELECT COUNT(*) FROM empty_test_customers GROUP BY floor(cast(discount AS integer));
+SELECT COUNT(*) FROM empty_test_customers GROUP BY substr(city, 1, id);
+SELECT COUNT(*) FROM empty_test_customers GROUP BY substr('aaaa', 1, 2);
+
+-- Get rejected because of subqueries
+SELECT COUNT(*), COUNT(x.city), COUNT(DISTINCT x.id)
+FROM (
+  SELECT * FROM empty_test_customers
+) x;
+
+SELECT COUNT(*), COUNT(x.city), COUNT(DISTINCT x.user_id)
+FROM (
+  SELECT y.city as city, y.id as user_id
+  FROM ( SELECT * FROM empty_test_customers ) y
+) x;
+
+SELECT x.user_city, COUNT(*), COUNT(DISTINCT x.id), COUNT(DISTINCT x.cid)
+FROM (
+  SELECT id, cid, city as user_city
+  FROM empty_test_customers
+  INNER JOIN test_purchases tp ON id = cid
+) x
+GROUP BY 1;
+
+SELECT COUNT(DISTINCT x.modified_id) FROM ( SELECT id AS modified_id FROM empty_test_customers ) x;
+
+-- Get rejected because of subqueries, but used to be rejected because of their inner aggregation
+SELECT * FROM ( SELECT COUNT(*) FROM empty_test_customers ) x;
+
+SELECT COUNT(city)
+FROM (
+  SELECT city FROM empty_test_customers
+  GROUP BY 1
+) x;
+
+-- Get rejected because of JOINs
+SELECT COUNT(*), COUNT(DISTINCT id), COUNT(DISTINCT cid) FROM empty_test_customers
+  INNER JOIN test_purchases tp ON id = cid;
+
+SELECT COUNT(c.city), COUNT(p.name) FROM empty_test_customers c
+  LEFT JOIN test_purchases ON c.id = cid
+  LEFT JOIN test_products p ON pid = p.id;
+
+SELECT city, COUNT(price) FROM empty_test_customers, test_products GROUP BY 1;
+
+SELECT city, COUNT(price) FROM test_products, empty_test_customers GROUP BY 1;
+
+SELECT city, COUNT(price) FROM test_products CROSS JOIN empty_test_customers GROUP BY 1;
+
+-- Get rejected because of WHERE
+SELECT COUNT(*) FROM empty_test_customers WHERE city = 'London';
