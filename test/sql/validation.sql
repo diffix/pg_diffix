833dbbd477c8350c4b0f6a97e4b8a99cfabed71b
pdobacz <5735525+pdobacz@users.noreply.github.com>
2022-03-02 09:23:44 +0100

    Use empty_test_customers to test untrusted mode validations

--- test/sql/validation.sql
+++ test/sql/validation.sql
@@ -128,25 +128,25 @@ SET pg_diffix.session_access_level = 'publish_untrusted';
 SELECT diffix.access_level();
 
 -- Get accepted
-SELECT substring(city, 1, 2) from test_customers;
-SELECT floor(discount) from test_customers;
-SELECT ceil(discount) from test_customers;
-SELECT round(discount) from test_customers;
-SELECT discount from test_customers;
-SELECT diffix.floor_by(discount, 2) from test_customers;
-SELECT diffix.round_by(discount, 2) from test_customers;
-SELECT diffix.ceil_by(discount, 2) from test_customers;
-SELECT diffix.floor_by(discount, 20) from test_customers;
-SELECT diffix.floor_by(discount, 2.0) from test_customers;
-SELECT diffix.floor_by(discount, 0.2) from test_customers;
-SELECT diffix.floor_by(discount, 20.0) from test_customers;
-SELECT diffix.floor_by(discount, 50.0) from test_customers;
+SELECT substring(city, 1, 2) from empty_test_customers;
+SELECT floor(discount) from empty_test_customers;
+SELECT ceil(discount) from empty_test_customers;
+SELECT round(discount) from empty_test_customers;
+SELECT discount from empty_test_customers;
+SELECT diffix.floor_by(discount, 2) from empty_test_customers;
+SELECT diffix.round_by(discount, 2) from empty_test_customers;
+SELECT diffix.ceil_by(discount, 2) from empty_test_customers;
+SELECT diffix.floor_by(discount, 20) from empty_test_customers;
+SELECT diffix.floor_by(discount, 2.0) from empty_test_customers;
+SELECT diffix.floor_by(discount, 0.2) from empty_test_customers;
+SELECT diffix.floor_by(discount, 20.0) from empty_test_customers;
+SELECT diffix.floor_by(discount, 50.0) from empty_test_customers;
 
 -- Get rejected because of invalid generalization parameters
-SELECT substring(city, 2, 2) from test_customers;
-SELECT diffix.floor_by(discount, 3) from test_customers;
-SELECT diffix.floor_by(discount, 3.0) from test_customers;
-SELECT diffix.floor_by(discount, 5000000000.1) from test_customers;
+SELECT substring(city, 2, 2) from empty_test_customers;
+SELECT diffix.floor_by(discount, 3) from empty_test_customers;
+SELECT diffix.floor_by(discount, 3.0) from empty_test_customers;
+SELECT diffix.floor_by(discount, 5000000000.1) from empty_test_customers;
 
 -- Get rejected because of invalid generalizing functions
-SELECT width_bucket(discount, 2, 200, 5) from test_customers;
+SELECT width_bucket(discount, 2, 200, 5) from empty_test_customers;
