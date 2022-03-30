ff8dcdd14e60ddcc6be09529935b2e86f985ddde
pdobacz <5735525+pdobacz@users.noreply.github.com>
2022-03-30 10:07:42 +0200

    Rename enable_low_effect_detection to bucket_merging

--- pg_diffix/config.h
+++ pg_diffix/config.h
@@ -23,7 +23,7 @@ typedef struct DiffixConfig
   int top_count_min;
   int top_count_max;
 
-  bool enable_low_effect_detection;
+  bool bucket_merging;
 
   bool compute_suppress_bin;
   char *text_label_for_suppress_bin;
