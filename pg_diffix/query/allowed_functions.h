7fcbbf834b2a949d9ed45a5f35a4f985d19bf49e
pdobacz <5735525+pdobacz@users.noreply.github.com>
2022-04-21 14:40:01 +0200

    Disallow ceil and ceil_by in untrusted access level

--- pg_diffix/query/allowed_functions.h
+++ pg_diffix/query/allowed_functions.h
@@ -12,9 +12,10 @@ extern bool is_allowed_function(Oid funcoid);
 extern bool is_allowed_cast(Oid funcoid);
 
 /*
- * Returns whether the OID points to a UDF being a implicit_range function, e.g. `ceil_by(x, 2.0)`.
+ * Returns whether the OID points to a UDF being a implicit_range function, e.g. `ceil_by(x, 2.0)`,
+ * allowed when called in `publish_untrusted` access level.
  */
-extern bool is_implicit_range_udf(Oid funcoid);
+extern bool is_implicit_range_udf_untrusted(Oid funcoid);
 
 /*
  * Returns whether the OID points to a built-in substring function.
@@ -22,8 +23,9 @@ extern bool is_implicit_range_udf(Oid funcoid);
 extern bool is_substring_builtin(Oid funcoid);
 
 /*
- * Returns whether the OID points to a built-in implicit_range function, e.g. `ceil(x)`.
+ * Returns whether the OID points to a built-in implicit_range function, e.g. `ceil(x)`,
+ * allowed when called in `publish_untrusted` access level.
  */
-extern bool is_implicit_range_builtin(Oid funcoid);
+extern bool is_implicit_range_builtin_untrusted(Oid funcoid);
 
 #endif /* PG_DIFFIX_ALLOWED_FUNCTIONS_H */
